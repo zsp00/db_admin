1c11d81abcad4777a31fc8126a8b4fb08af62bf8
LuJin <345526407@qq.com>
2017-11-08 09:08:36 +0800

    '流程管理'

--- application/index/controller/Process.php
+++ application/index/controller/Process.php
@@ -94,22 +94,181 @@ class Process extends Common
 
 	/**
 	 * 获取流程列表
+	 * @param  int $page    当前页码
+	 * @param  int $listRow 每页显示记录数
 	 * @return array 流程列表信息
 	 */
-	public function getProcessList()
+	public function getProcessList($page=1, $listRow=12)
 	{
-		$result = model('Process')->where('status', 1)->select();
+		$result = model('Process')->where('status', 1)->page($page, $listRow)->select();
+		foreach ($result as $k => $v)
+		{
+			$result[$k]['dept'] = model('OrgDept')->getNameList($v['deptNo']);
+			$result[$k]['creator'] = model('UserEmp')->getUserRealName($v['creator']);
+			$result[$k]['lastModifier'] = model('UserEmp')->getUserRealName($v['lastModifier']);
+		}
+		$list['list'] = $result;
+		$list['total'] = model('Process')->where('status', 1)->count();
+		$list['listRow'] = $listRow;
+		$list['page'] = $page;
+		if ($result)
+			$this->success('', '', $list);
+		else
+			$this->error();
+	}
+
+	/**
+	 * 删除流程
+	 * @param  array $ids 要删除的流程Id组成的数组
+	 * @return array      删除结果
+	 */
+	public function del($ids)
+	{
+		$id = implode(',', $ids);
+		$resProcess = model('Process')->where(['id'=>['in', $id]])->delete();
+		$resProcessData = model('ProcessData')->where(['pId'=>['in', $id]])->delete();
+
+		if ($resProcess && $resProcessData)
+			$this->success();
+		else
+			$this->error();
+	}
+
+	/**
+	 * 修改流程时查询流程信息
+	 * @param  int $pId 流程Id
+	 * @return array      流程信息
+	 */
+	public function getProcessInfo($pId)
+	{
+		$result = model('Process')->where('id', $pId)->find();
+
 		if ($result)
 		{
-			foreach ($result as $k => $v)
+			$process = model('ProcessData')->where('pId', $pId)->select();
+			$stpts = array();
+			foreach ($process as $k => $v)
 			{
-				$result[$k]['dept'] = model('OrgDept')->getNameList($v['deptNo']);
-				$result[$k]['creator'] = model('UserEmp')->getUserRealName($v['creator']);
-				$result[$k]['lastModifier'] = model('UserEmp')->getUserRealName($v['lastModifier']);
+				$audit_user = json_decode($v['audit_user']);
+				$step = array();
+				foreach ($audit_user as $kk => $vv)
+				{
+					if ($kk == 'notIn')
+						$step['notIn'] = $vv;
+					else
+						$step['auditor'] = $vv;
+				}
+				$step['describe'] = $v['pDescribe'];
+				$steps[] = $step;
 			}
+			$result['process'] = $steps;
+			$result['compNo'] = model('OrgDept')->where('DEPT_NO', $result['deptNo'])->value('COMP_NO');
 			$this->success('', '', $result);
 		}
 		else
 			$this->error();
+	}
+
+	/**
+	 * 更新流程
+	 * @param  array $data 更新流程的数据
+	 * @return array       更新结果
+	 */
+	public function edit($data)
+	{
+		$member = session('member');
+		unset($data['process'][0]);
+
+		$updateProcess = array(
+			'name'			=>	trim($data['name']),
+			'deptNo'		=>	$data['deptValue'],
+			'level'			=>	count($data['process']),
+			'lastModifier'	=>	$member['EMP_NO']
+		);
+
+		// Db::startTrans();
+		$resProcess = model('Process')->save($updateProcess, ['id'=>$data['id']]);
+		if ($resProcess !== false)
+		{
+			$pId = $data['id'];
+			$oldStepCount = model('ProcessData')->where('pId', $pId)->count();
+			// 如果更新后流程步骤数量等于原步骤数量，直接更新
+			// 如果更新后流程步骤数量大于原步骤数量，修改原有的数量，多出来的插入
+			if (count($data['process']) >= $oldStepCount)
+			{
+				foreach ($data['process'] as $k => $v)
+				{
+					$audit = array(
+						'dept'	=>	$v['auditor'],
+						'notIn'	=>	$v['notIn']
+					);
+					$update = array(
+						'pId'		=>	$pId,
+						'levelNo'	=>	$k,
+						'audit_user'=>	json_encode($audit),
+						'pDescribe'	=>	strip_tags(trim($v['describe']))
+					);
+					$where = array(
+						'pId'		=>	$pId,
+						'levelNo'	=>	$k
+					);
+					if ($k <= $oldStepCount)    // 小于等于原来的步骤执行更新
+					{
+						if (model('ProcessData')->save($update, $where) === false)
+						{
+							Db::rollback();
+							$this->error();
+						}
+					}
+					else    // 多出原来的步骤执行插入
+					{
+						if (model('ProcessData')->isUpdate(false)->save($update) === false)
+						{
+							Db::rollback();
+							$this->error();
+						}
+					}
+				}
+			}
+			// 如果更新后流程步骤数量小于原步骤数量，修改更新后的数量，原来多出来的删除
+			else
+			{
+				foreach ($data['process'] as $k => $v)
+				{
+					$audit = array(
+						'dept'	=>	$v['auditor'],
+						'notIn'	=>	$v['notIn']
+					);
+					$update = array(
+						'pId'		=>	$pId,
+						'levelNo'	=>	$k,
+						'audit_user'=>	json_encode($audit),
+						'pDescribe'	=>	strip_tags(trim($v['describe']))
+					);
+					$where = array(
+						'pId'		=>	$pId,
+						'levelNo'	=>	$k
+					);
+					if (model('ProcessData')->save($update, $where) === false)
+					{
+						Db::rollback();
+						$this->error();
+					}
+				}
+				if (model('ProcessData')->where(['pId'=>$pId, 'levelNo'=>['>', count($data['process'])]])->delete() === false)
+				{
+					Db::rollback();
+					$this->error();
+				}
+			}
+
+			Db::commit(); 
+			$this->success();
+		}
+		else
+		{
+			Db::rollback();
+			$this->error();
+		}
 	}
 }
