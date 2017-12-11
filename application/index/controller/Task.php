a523a282b78774b6dbc8d475477a558179150a67
litianhang <litianhang@bmzymtr.com>
2017-12-11 18:28:14 +0800

    提交提示

--- application/index/controller/Task.php
+++ application/index/controller/Task.php
@@ -151,9 +151,9 @@ class Task extends Common
         if ($updateDataStatus === false) {
             $this->error($TaskDataModel->getError());
         }
-         if($updateDataStatus === 0 && $taskUpdate === 0){
-             $this->error('您没做任何修改！');
-         }
+//         if($updateDataStatus === 0 && $taskUpdate === 0){
+//             $this->error('您没做任何修改！');
+//         }
         $TaskLogModel = new TaskLog();
         $result = $TaskLogModel->addLog($taskInfo['id'],$taskDataInfo['id'],'edit',$userInfo['EMP_NO'],$deptNo,$update,$taskDataInfo->toArray());
 
@@ -171,21 +171,18 @@ class Task extends Common
      * @param  int $nextLevel    当前流程的下一步
      * @return array               提交结果
      */
-    public function submits($id, $pId, $currentLevel, $nextLevel, $completeSituation='', $problemSuggestions='', $analysis='')
+    public function submits($id, $pId, $currentLevel, $nextLevel)
     {
-        $TaskDataModel = new TaskData();
-        $taskDataInfo = $TaskDataModel->where(['id'=>$id])->find();
-        if(!$taskDataInfo){
-            $this->error('该条记录未找到');
-        }
-        if($taskDataInfo['completeSituation'] != $completeSituation || $taskDataInfo['problemSuggestions'] != $problemSuggestions || $taskDataInfo['analysis'] != $analysis){
-            $this->error('请先保存！');
-        }
         //获取用户的信息
         $userInfo = getUserInfo();
         $deptNo = Model('OrgDept')->getDeptNo($userInfo['DEPTNO']);
+        $TaskDataModel = new TaskData();
+        $taskDataInfo = $TaskDataModel->where(['id'=>$id])->find();
+        if(!$taskDataInfo)
+            $this->error('该条记录未找到');
         // 获取流程总的等级
         $level = Model('Process')->where('id', $taskDataInfo['pId'])->value('level');
+
         //如果部门办事员和主任都在流程1  2 级那么跳过2级到3级
         $identitys = Model('ProcessData')->getStepIds($taskDataInfo['pId']);
         if(count($identitys) > 1 && ($identitys['0'] == 1 && $identitys['1'] == 2)){
